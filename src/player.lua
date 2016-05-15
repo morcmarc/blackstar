9ddd2a1fa121ef71000ebff21329171fc16ec40f
Marcell Jusztin <marcell.jusztin@flashtalking.com>
2016-05-15 20:25:00 +0100

    walking

--- src/player.lua
+++ src/player.lua
@@ -1,31 +1,32 @@
 local sodapop = require "vendor.sodapop.sodapop"
 
 local player = {
-    x       = 0,
-    y       = 0,
+    x       = 300,
+    y       = 600-128-64+7,
     sprite  = nil,
 }
 
 function player.init()    
     player.sprite = sodapop.newAnimatedSprite(64, 64)
-    player.sprite:addAnimation("stand", {
-        image       = love.graphics.newImage "assets/sprites/hero_stand.png",
+    player.sprite:addAnimation("walk", {
+        image       = love.graphics.newImage "assets/sprites/hero_walk.png",
         frameWidth  = 128,
         frameHeight = 128,
         frames      = {
-            {1, 1, 3, 1, .20}
+            {1, 1, 4, 1, .06}
         },
     })
 end
 
 function player.draw()
     love.graphics.push()
-        love.graphics.translate(300, 600-128-64+7)
+        love.graphics.translate(player.x, player.y)
         player.sprite:draw()
     love.graphics.pop()
 end
 
 function player.update(dt)
+    player.x = player.x + 4
     player.sprite:update(dt)
 end
 
